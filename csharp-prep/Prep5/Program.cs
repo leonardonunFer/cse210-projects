1244d387af77be4586af2c4efb1c61a782895c37
Leonardo <leonesmas2005@gmail.com>
2023-04-26 23:22:59 -0300

    funciones

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -1,9 +1,19 @@
 using System;
 
-class Program
-{
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep5 World!");
+class Program {
+    static void Main(string[] args) {
+        Console.WriteLine("Welcome to the program!");
+        Console.Write("Please enter your name: ");
+        string name = Console.ReadLine();
+
+        Console.Write("Please enter your favorite number: ");
+        string favoriteNumberStr = Console.ReadLine();
+        int favoriteNumber = int.Parse(favoriteNumberStr);
+
+        PrintAnswers(name, favoriteNumber);
+    }
+
+    static void PrintAnswers(string name, int favoriteNumber) {
+        Console.WriteLine($"Your name is: {name}, and your favorite number is: {favoriteNumber}.");
     }
 }
