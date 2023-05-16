83ae3ed872d474bcccabe107063e992ca0e86d74
Leonardo <leonesmas2005@gmail.com>
2023-05-15 23:32:55 -0300

    fraction

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,34 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        // Console.WriteLine("Hello Learning03 World!");
+
+        // Step 5 Getters and Setters
+        // Fraction f1 = new Fraction();
+        // f1.SetTop(3);
+        // f1.SetBottom(4);
+
+        // Console.WriteLine(f1.GetTop());
+        // Console.WriteLine(f1.GetBottom());
+
+        // Step 6 return methods
+        Fraction f1 = new Fraction();
+        Console.WriteLine(f1.GetFractionString());
+        Console.WriteLine(f1.GetDecimalValue());
+
+        Fraction f2 = new Fraction(5);
+        Console.WriteLine(f2.GetFractionString());
+        Console.WriteLine(f2.GetDecimalValue());
+
+        Fraction f3 = new Fraction(3, 4);
+        Console.WriteLine(f3.GetFractionString());
+        Console.WriteLine(f3.GetDecimalValue());
+
+        Fraction f4 = new Fraction(1, 3);
+        Console.WriteLine(f4.GetFractionString());
+        Console.WriteLine(f4.GetDecimalValue());
+
+        
+
     }
 }
